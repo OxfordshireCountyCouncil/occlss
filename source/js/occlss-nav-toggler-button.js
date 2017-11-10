0e62f9cf1938d49f8396a46d5646dbb68726a98d
vagrant <vagrant@drupalvm.dev>
2017-11-10 13:06:25 +0000

    Mobile menu expand Javascript

---
+++ source/js/occlss-nav-toggler-button.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+  $('.js-occlss-nav-toggler-button').click(function () {
+
+    if ($(this).hasClass('is-active')) {
+      $(this).parents().children('.occlss-site-nav__list').removeClass('is-active');
+      $(this).removeClass('is-active');
+    } else {
+      $(this).parents().children('.occlss-site-nav__list').addClass('is-active');
+      $(this).addClass('is-active');
+    }
+  });
+});

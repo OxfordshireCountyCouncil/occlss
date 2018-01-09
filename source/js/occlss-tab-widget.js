0c304e4ad8d68f98112fbc550decd87f9de5faa7
Arty <artiom.sobolev@oxfordshire.gov.uk>
2018-01-09 15:14:55 +0000

    Tab widget javascript adjusted to have mobile links

--- source/js/occlss-tab-widget.js
+++ source/js/occlss-tab-widget.js
@@ -3,7 +3,17 @@
 
     var $tabWidget = $('.js-occlss-tab-widget');
 
-    var setupTabs = function($tab, $allTabs, $tabPanels, $tabListItems, i) {
+    var setupTabs = function ($tab, $allTabs, $tabPanels, $tabListItems, $mobileTab, $mobileTabs, i) {
+        $mobileTab = $($mobileTab);
+
+        $mobileTab
+            .attr({
+                'id': 'tab-link-' + i,
+                'tabindex': '-1',
+                'role': 'tab',
+                'aria-selected': 'false',
+                'aria-controls': 'tab-panel-' + i
+            });
         $tab
             .attr({
                 'id': 'tab-link-' + i,
@@ -13,8 +23,15 @@
                 'aria-controls': 'tab-panel-' + i
             });
 
-        if (i === 0) {
+        if ($tab.hasClass("is-active")) {
             $tab
+                .attr({
+                    'tabindex': '0',
+                    'aria-selected': 'true',
+                    'aria-describedby': 'occlss-tab-widget-description'
+                })
+                .addClass('is-active');
+            $($mobileTab)
                 .attr({
                     'tabindex': '0',
                     'aria-selected': 'true',
@@ -23,22 +40,30 @@
                 .addClass('is-active');
         }
 
-        $tab.on('click', function(e) {
+        $mobileTab.on('click', function (e) {
             e.preventDefault();
-
-            tabClick($(this),  $allTabs, $tabPanels, i);
+            mtabClick($(this), $allTabs, $tabPanels, $mobileTabs, i);
         });
-      
-        $tab.on('focus', function(e) {
-            tabClick($(this),  $allTabs, $tabPanels, i);
+
+        $tab.on('click', function (e) {
+            e.preventDefault();
+            tabClick($(this), $allTabs, $tabPanels, $mobileTabs, i);
+        });
+
+        $tab.on('focus', function (e) {
+            tabClick($(this), $allTabs, $tabPanels, $mobileTabs, i);
         });
 
         $tab.on('keydown', function (e) {
             tabKeydown($(this), $allTabs, $tabPanels, $tabListItems, i, e);
         });
+
+        if ($tab.hasClass("is-active") ) {
+            tabClick($tab, $allTabs, $tabPanels, $mobileTabs, i);
+        }
     };
 
-    var setupTabPanels = function(tabPanel, i) {
+    var setupTabPanels = function (tabPanel, i) {
         tabPanel
             .attr({
                 'id': 'tab-panel-' + i,
@@ -47,14 +72,24 @@
                 'aria-labelledby': 'tab-link-' + i
             });
 
-        if (i === 0) {
+        if (tabPanel.class === "is-active") {
             tabPanel
                 .attr('aria-hidden', 'false')
                 .addClass('is-active');
         }
     };
 
-    var tabClick = function($thisTab, $allTabs, $tabPanels, i) {
+    var tabClick = function ($thisTab, $allTabs, $tabPanels, $mobileTabs, i) {
+        var $cmTab = $($mobileTabs[i]);
+
+        $mobileTabs
+            .attr({
+                'tabindex': -1,
+                'aria-selected': 'false'
+            })
+            .removeAttr('aria-describedby')
+            .removeClass('is-active');
+
         $allTabs
             .attr({
                 'tabindex': -1,
@@ -62,6 +97,13 @@
             })
             .removeAttr('aria-describedby')
             .removeClass('is-active');
+        $cmTab
+            .attr({
+                'tabindex': 0,
+                'aria-selected': 'true',
+                'aria-describedby': 'occlss-tab-widget-description'
+            })
+            .addClass('is-active');
 
         $thisTab
             .attr({
@@ -80,14 +122,59 @@
             .addClass('is-active');
     };
 
-    var tabKeydown = function($thisTab, $allTabs, $tabPanels, $tabListItems, i, e) {
+    var mtabClick = function ($thisTab, $allTabs, $tabPanels, $mobileTabs, i) {
+        
+        var $cTab = $($allTabs[i]);
+
+        $mobileTabs
+            .attr({
+                'tabindex': -1,
+                'aria-selected': 'false'
+            })
+            .removeAttr('aria-describedby')
+            .removeClass('is-active');
+
+        $allTabs
+            .attr({
+                'tabindex': -1,
+                'aria-selected': 'false'
+            })
+            .removeAttr('aria-describedby')
+            .removeClass('is-active');
+        
+        $cTab
+            .attr({
+                'tabindex': 0,
+                'aria-selected': 'true',
+                'aria-describedby': 'occlss-tab-widget-description'
+            })
+            .addClass('is-active');
+
+        $thisTab
+            .attr({
+                'tabindex': 0,
+                'aria-selected': 'true',
+                'aria-describedby': 'occlss-tab-widget-description'
+            })
+            .addClass('is-active');
+
+        $tabPanels
+            .attr('aria-hidden', 'true')
+            .removeClass('is-active');
+
+        $tabPanels.eq(i)
+            .attr('aria-hidden', 'false')
+            .addClass('is-active');
+    };
+
+    var tabKeydown = function ($thisTab, $allTabs, $tabPanels, $tabListItems, i, e) {
         var keyCode = e.which,
             $nextTab = $thisTab.parent().next().is('li') ? $thisTab.parent().next().find('a') : false,
             $previousTab = $thisTab.parent().prev().is('li') ? $thisTab.parent().prev().find('a') : false,
             $firstTab = $thisTab.parent().parent().find('li:first').find('a'),
             $lastTab = $thisTab.parent().parent().find('li:last').find('a');
 
-        switch(keyCode) {
+        switch (keyCode) {
             // Left/Up
             case 37:
             case 38:
@@ -145,30 +232,30 @@
     };
 
     $tabWidget.each(function () {
+
+        // Get All the objects
         var $this = $(this),
             $tabList = $this.find('> ul'),
             $tabListItems = $tabList.find('li'),
-            $tabMobItems = $this.find('.js-occlss-mob-tab'),
             $allTabs = $tabListItems.find('a'),
+            $mobileTabs = $('.js-occlss-mob-tab'),
             $tabPanels = $this.find('> div > div');
-
 
         $tabList.attr('role', 'tablist');
         $tabListItems.attr('role', 'presentation');
 
         $allTabs.each(function (i) {
-            setupTabs($(this), $allTabs, $tabPanels, $tabListItems, i);
-        });
-
-        $tabMobItems.each(function (i) {
-            setupTabs($(this), $tabMobItems, $tabPanels, $tabListItems, i);
+            setupTabs($(this), $allTabs, $tabPanels, $tabListItems, $mobileTabs[i], $mobileTabs, i);
         });
 
         $tabPanels.each(function (i) {
             setupTabPanels($(this), i);
         });
+
+
     });
 
-  $('html').addClass('js').removeClass('no-js');
+
+    $('html').addClass('js').removeClass('no-js');
 
 })(document, window, jQuery);

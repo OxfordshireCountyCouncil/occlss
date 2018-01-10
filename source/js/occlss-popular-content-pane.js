e780bdc665fa7cb700fb3c58090a36d65419f5b0
Arty <artiom.sobolev@oxfordshire.gov.uk>
2018-01-10 08:38:37 +0000

    Popular link javascript optimised

--- source/js/occlss-popular-content-pane.js
+++ source/js/occlss-popular-content-pane.js
@@ -1,30 +1,37 @@
-$(document).ready(function () {
+(function (document, window, $, undefined) {
+  'use strict';
+
+  var $popularContentPanes = $('.js-occlss-popular-content-pane');
   
-  $(".js-occlss-popular-content-pane").on('focus', function () {
-    if (!$(this).data("mouseDown"))
-      $(this).click();
+  var popularContentPaneSetup = function($pane, $allPanes, i) {
+      $pane.on('focus', function () {
+        if (!$(this).data("mouseDown"))
+          $(this).click();
+      });
+      $pane.on('mousedown', function () {
+        $(this).data("mouseDown", true);
+      });
+      $pane.on('mouseup', function () {
+        $(this).removeData("mouseDown");
+      });
+      $pane.on('click', function (e) {
+        if ($(this).hasClass('is-active')) {
+          //Close the current section
+          $allPanes.removeClass('is-active');
+          $('.collapsing-section').slideUp();
+        } else {
+          //close the prev section & open the newly click
+          $allPanes.removeClass('is-active');
+          $('.collapsing-section').slideUp(); //Side up all sections that are open & remove their open class
+          $(this).addClass('is-active');
+          var sectionToOpen = $(this).next('.collapsing-section');
+          $(sectionToOpen).slideDown();
+        }
+      });
+  };
+  $popularContentPanes.each(function (i) {
+      var $this = $(this);
+      popularContentPaneSetup($(this), $popularContentPanes, i)
   });
 
-  $(".js-occlss-popular-content-pane").on('mousedown', function () {
-    $(this).data("mouseDown", true);
-  });
-
-  $(".js-occlss-popular-content-pane").on('mouseup', function () {
-    $(this).removeData("mouseDown");
-  });
-
-  $(".js-occlss-popular-content-pane").on('click', function (e) {
-    if ($(this).hasClass('is-active')) {
-      //Close the current section
-      $('.js-occlss-popular-content-pane').removeClass('is-active');
-      $('.collapsing-section').slideUp();
-    } else {
-      //close the prev section & open the newly click
-      $('.js-occlss-popular-content-pane').removeClass('is-active');
-      $('.collapsing-section').slideUp(); //Side up all sections that are open & remove their open class
-      $(this).addClass('is-active');
-      var sectionToOpen = $(this).next('.collapsing-section');
-      $(sectionToOpen).slideDown();
-    }
-  });
-});
+})(document, window, jQuery);

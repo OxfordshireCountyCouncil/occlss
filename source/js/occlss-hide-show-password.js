1dc588aa9b4c4500f6f1ff87f1b42d1e0e3fac25
Arty <artiom.sobolev@oxfordshire.gov.uk>
2018-02-13 10:51:24 +0000

    Forms variables restructure

--- source/js/occlss-hide-show-password.js
+++ source/js/occlss-hide-show-password.js
@@ -5,11 +5,11 @@ $(document).ready(function () {
     var imgVisible = getImage.replace("invisible", "visible");
 
     if( $(this).hasClass('show') ) {
-      $(this).parents('.js-occlss-group-password').children('.occlss-form-cntrs__input').attr('type','password');
+      $(this).parents('.js-occlss-group-password').children('.occlss-form-cntrls__input').attr('type','password');
       $(this).children().children('svg').children().attr("xlink:href",imgVisible);
       $(this).removeClass('show');
     } else {
-       $(this).parents('.js-occlss-group-password').children('.occlss-form-cntrs__input').attr('type','text');
+       $(this).parents('.js-occlss-group-password').children('.occlss-form-cntrls__input').attr('type','text');
        $(this).children().children('svg').children().attr("xlink:href",imgInVisible);
        $(this).addClass('show');
     }
@@ -20,6 +20,6 @@ $(document).ready(function () {
     var imgInVisible = getImage.replace("visible", "invisible");
     $('.js-occlss-hide-show').children().children('svg').children().attr("xlink:href",imgVisible);
 		$('.js-occlss-hide-show').addClass('show');
-		$('.js-occlss-hide-show').parents('.js-occlss-group-password').children('.occlss-form-cntrs__input').attr('type','password');
+		$('.js-occlss-hide-show').parents('.js-occlss-group-password').children('.occlss-form-cntrls__input').attr('type','password');
   }); 
 });

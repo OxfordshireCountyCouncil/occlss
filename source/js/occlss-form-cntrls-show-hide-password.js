8a017b666985be698074bcd02bc78c751bd2ecdd
Arty <artiom.sobolev@oxfordshire.gov.uk>
2018-02-13 13:18:49 +0000

    Show hide password updates

--- source/js/occlss-form-cntrls-show-hide-password.js
+++ source/js/occlss-form-cntrls-show-hide-password.js
@@ -1,15 +1,15 @@
 $(document).ready(function () {
-  $('.js-occlss-hide-show').click(function() {
+  $('.js-occlss-form-cntrls-show-hide-password').click(function() {
     var getImage = $(this).children().children('svg').children().attr("xlink:href");
     var imgInVisible = getImage.replace("visible", "invisible");
     var imgVisible = getImage.replace("invisible", "visible");
 
     if( $(this).hasClass('show') ) {
-      $(this).parents('.js-occlss-group-password').children('.occlss-form-cntrls__input').attr('type','password');
+      $(this).parents('.js-occlss-form-cntrls-password').children('.occlss-form-cntrls__input').attr('type','password');
       $(this).children().children('svg').children().attr("xlink:href",imgVisible);
       $(this).removeClass('show');
     } else {
-       $(this).parents('.js-occlss-group-password').children('.occlss-form-cntrls__input').attr('type','text');
+       $(this).parents('.js-occlss-form-cntrls-password').children('.occlss-form-cntrls__input').attr('type','text');
        $(this).children().children('svg').children().attr("xlink:href",imgInVisible);
        $(this).addClass('show');
     }
@@ -18,8 +18,8 @@ $(document).ready(function () {
 	$('#js-occlss-form-password-show-hide button[type="submit"]').on('click', function(event){
     var getImage = $(this).children().children('svg').children().attr("xlink:href");
     var imgInVisible = getImage.replace("visible", "invisible");
-    $('.js-occlss-hide-show').children().children('svg').children().attr("xlink:href",imgVisible);
-		$('.js-occlss-hide-show').addClass('show');
-		$('.js-occlss-hide-show').parents('.js-occlss-group-password').children('.occlss-form-cntrls__input').attr('type','password');
+    $('.js-occlss-form-cntrls-show-hide-password').children().children('svg').children().attr("xlink:href",imgVisible);
+		$('.js-occlss-form-cntrls-show-hide-password').addClass('show');
+		$('.js-occlss-form-cntrls-show-hide-password').parents('.js-occlss-form-cntrls-password').children('.occlss-form-cntrls__input').attr('type','password');
   }); 
 });

8ae142959ecfd06f76923010e859c3c785ad2c0e
Artiom Sobolev <artiomsobolev@MacBook-Pro.mshome.net>
2018-05-22 15:01:45 +0100

    Category nav new functionality

--- source/js/occlss-category-nav.js
+++ source/js/occlss-category-nav.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-  $('html').on('click', '.js-occlss-category-nav .is-active .occlss-category-nav__link', function(event){
+  $('html').on('click', '.js-occlss-category-nav .is-disabled .occlss-category-nav__link', function(event){
     event.preventDefault();
     event.stopPropagation();
   });
